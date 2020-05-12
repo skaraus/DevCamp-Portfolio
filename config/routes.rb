df6dfaeaf51f4fa7114ddffcf1287ea8e399b6ec
Scott Karaus <jskaraus@gmail.com>
2020-05-12 13:02:24 -0500

    Implmented Custom Action on Blogs

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,19 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do 
+  	member do 
+  		get :toggle_status
+  	end
+  end
+
 
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
 
   
+
+
+
 end
