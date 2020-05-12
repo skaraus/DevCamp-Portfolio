68b0bed85bcbfb3303a789d5f10d37f7c00a8b02
Scott Karaus <jskaraus@gmail.com>
2020-05-12 12:36:28 -0500

    Implemented Status via Enum on Blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,10 @@
 class Blog < ApplicationRecord
+
+	# Blog Post Status Enum
+	enum status: {draft: 0, published: 1}
+
+
+	# Implement Friendly Slugs for Blogs
 	extend FriendlyId
 	friendly_id :title, use: :slugged
 end
